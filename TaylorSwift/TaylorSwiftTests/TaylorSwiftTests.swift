432df4a73943f0bb60b95a3d5363fc6ab16c1784
JesseScott <jessecolinscott@gmail.com>
2015-04-22 09:11:04 -0700

    Xcode Project

---
+++ TaylorSwift/TaylorSwiftTests/TaylorSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  TaylorSwiftTests.swift
+//  TaylorSwiftTests
+//
+//  Created by Jesse Scott on 2015-04-22.
+//  Copyright (c) 2015 Jesse Scott. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TaylorSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
