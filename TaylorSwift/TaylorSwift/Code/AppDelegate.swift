f4e55486be737bc5d930b2e81810250a7af0c19b
JesseScott <jessecolinscott@gmail.com>
2015-09-18 21:09:34 -0700

    basic project setup

---
+++ TaylorSwift/TaylorSwift/Code/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  TaylorSwift
+//
+//  Created by Jesse Scott on 2015-04-22.
+//  Copyright (c) 2015 Jesse Scott. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+       
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+
+    }
+
+
+}
+
